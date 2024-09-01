442e04b754073ae4aafa2ce758c900446f243f08
SentienceZero <160515658+SentienceZero@users.noreply.github.com>
2024-09-01 18:54:32 +0800

    Minor test update that displays the frontend

--- contactlist/models.py
+++ contactlist/models.py
@@ -4,7 +4,8 @@ from django.db import models
 # Make sure to run makemigrations and migrate commands to link such models
 
 class Contact(models.Model):
-    companyName   = models.CharField(max_length=120)
-    description   = models.TextField(blank=True, null=True)
-    contactNumber = models.PositiveSmallIntegerField()
-    status        = models.BooleanField('Online')
+    cname           = models.CharField(max_length=120)
+    caddress        = models.TextField(blank=False, null=False)
+    cdescription    = models.TextField(blank=False, null=False)
+    cnumber         = models.PositiveSmallIntegerField()
+    cstatus         = models.BooleanField('Online')
--- contactlist/views.py
+++ contactlist/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Contact
 
 # Create your views here. ;; Request Handler
 
@@ -9,4 +10,8 @@ def say_hello(request):
 def homepage_view(request, *args, **kwargs):
     print(args, kwargs)
     print(request.user)
-    return render(request, 'base.html', {})
+    obj = Contact.objects.get(id=1)
+    context = {
+        'object':obj
+    }
+    return render(request, 'base.html', context)

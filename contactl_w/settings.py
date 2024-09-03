83dcf80944e7d20ca13570a7994e49fdb8411151
SentienceZero <160515658+SentienceZero@users.noreply.github.com>
2024-09-03 17:42:55 +0800

    Added max length validators in input fields, and a phone number validator

--- contactl_w/settings.py
+++ contactl_w/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'contactlist',
     'debug_toolbar',
+    'phonenumbers',
 ]
 
 MIDDLEWARE = [
--- contactlist/views.py
+++ contactlist/views.py
@@ -5,35 +5,50 @@ from .forms import AddForm
 
 # Create your views here. ;; Request Handler
 
-    # Raw HTML form method
-    # form = AddForm(request.POST or None)
-    # if form.is_valid():
-    #    form.save()
-    #    form = AddForm()
-    # context = {
-    #   'form':form
-    # }
-    
-def contact_create_view(request):
-    form = AddForm()
-    if request.method == 'POST':
-        form = AddForm(request.POST)
-        if form.is_valid():
-            print(form.cleaned_data)
-            # Pass in verified data with ** to convert to args
-            Contact.objects.create(**form.cleaned_data)
-        else :
-            print(form.errors)
-    context = {
-        'form':form
-    }
-    return render(request, 'addForm.html', context)
+# Raw HTML form method
+# form = AddForm(request.POST or None)
+# if form.is_valid():
+#    form.save()
+#    form = AddForm()
+# context = {
+#   'form':form
+# }
+
+# def contact_create_view(request):
+#     form = AddForm()
+#     if request.method == 'POST':
+#         form = AddForm(request.POST)
+#         if form.is_valid():
+#             print(form.cleaned_data)
+#             # Pass in verified data with ** to convert to args
+#             Contact.objects.create(**form.cleaned_data)
+#         else :
+#             print(form.errors)
+#     context = {
+#         'form':form
+#     }
+#     return render(request, 'addForm.html', context)
 
 def homepage_view(request, *args, **kwargs):
     print(args, kwargs)
     print(request.user)
-    obj = Contact.objects.get(id=1)
+    obj = Contact.objects.get(id=10)
     context = {
         'object':obj
     }
     return render(request, 'base.html', context)
+
+def contact_create_view(request):
+    form = AddForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = AddForm()
+    context = {
+    'form':form
+    }
+    return render(request, 'addForm.html', context)
+
+# def homepage_view(request, *args, **kwargs):
+#     print(args, kwargs)
+#     print(request.user)
+#     return render(request, 'homepage.html')
